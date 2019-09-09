24b97f01f887d50fcb95a7cfedfb26559dfa83c1
yokarak <ande67f3@edu.easj.dk>
2019-09-09 10:31:20 +0200

    random try out of different variabler and smth. else

--- src/VariablerBook.java
+++ src/VariablerBook.java
@@ -1,7 +1,46 @@
+
+
 public class VariablerBook {
-
-    //2.9 numeric Data Types and operations
-    
+    public static void main(String[] args) {
 
 
+        //2.9 numeric Data Types and operations
+
+        byte a1 = 12;
+        short b1 = 32001;
+
+        int x = a1 + b1;
+        System.out.println("x * a1^5 =");
+        System.out.println( x * a1^5);
+
+
+        /*
+        java.util.Scanner {
+            int m = 1;
+            int t = 2;
+            int o = 3;
+            int t2 = 4;
+            int f = 5;
+            int l = 6;
+            int s = 7;
+
+            int day = (6 + o) % 7;
+            System.out.println(day);
+*/
+
+        float x1 = 1/3f;
+        float x2 = 2/4f;
+        double x3;
+        float x4;
+        x3 = x1 + x2;
+        x4 = x1 + x2;
+        System.out.println(x3);
+        System.out.println(x4);
+        // float rounds up and thereby gaining more value than before
+
+
+
+
+
+    }
 }
